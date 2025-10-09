0f7748f6caa3a53a819afb7b6a1ca2fec44d6ed2
Artiom Stadnijchuk <artiomstadnijchuk@gmail.com>
2025-10-09 22:05:22 +0500

    первая реализация spring - cloud

--- core/main-service/src/main/java/ru/practicum/MainService.java
+++ core/main-service/src/main/java/ru/practicum/MainService.java
@@ -3,11 +3,12 @@ package ru.practicum;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
 @EnableDiscoveryClient
-public class StatServer {
+@SpringBootApplication
+public class MainService {
     public static void main(String[] args) {
-        SpringApplication.run(StatServer.class, args);
+        SpringApplication.run(MainService.class, args);
     }
 }
--- infra/gateway-server/src/main/java/ru/practicum/Gateway.java
+++ infra/gateway-server/src/main/java/ru/practicum/Gateway.java
@@ -2,12 +2,13 @@ package ru.practicum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@ComponentScan("ru.practicum")
+
+@EnableDiscoveryClient
 @SpringBootApplication
-public class MainService {
+public class Gateway {
     public static void main(String[] args) {
-        SpringApplication.run(MainService.class, args);
+        SpringApplication.run(Gateway.class, args);
     }
 }
